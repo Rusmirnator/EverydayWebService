aa2507d0877506b3aa65c026f4d1e10f167960e5
Marcin Rusinek <m.rusinek@logifact.pl>
2022-08-10 17:14:09 +0200

    Initial commit on new branch.

---
+++ Everyday.Core/Models/ConsumableDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everyday.Core.Models
+{
+    public class ConsumableDTO
+    {
+        public int Id { get; set; }
+    }
+}
