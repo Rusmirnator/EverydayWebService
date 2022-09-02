b2513e0d997e9f2a374010f6ff48ec15b5e559e5
Marcin Rusinek <m.rusinek@logifact.pl>
2022-09-02 22:30:09 +0200

    Reconfigured host.

--- Everyday.API/Program.cs
+++ Everyday.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Everyday.API
 {
@@ -12,14 +14,33 @@ namespace Everyday.API
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder
+                    .UseUrls(BuildHostingUrls())
+                        .UseStartup<Startup>();
+            });
+
+        private static string[] BuildHostingUrls()
+        {
+            string[] hostingUrls = new string[2];
+
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+
+                hostingUrls[0] = string.Concat("https://", endPoint.Address, ":", "5001");
+                hostingUrls[1] = string.Concat("http://", endPoint.Address, ":", "5000");
+            }
+            return hostingUrls;
+        }
     }
 }
